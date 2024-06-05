8ae868af26f6da470e37bdb684a1a971ff5da745
장영욱 <raeben@naver.com>
2024-06-05 10:31:41 +0900

    option api set

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,7 @@ require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
     root: true,
-    extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/eslint-config-prettier'],
+    extends: ['plugin:vue/vue3-essential', 'eslint:recommended'],
     parserOptions: {
         ecmaVersion: 'latest'
     },
@@ -13,7 +13,7 @@ module.exports = {
         'vue/component-tags-order': [
             'error',
             {
-                order: ['script', 'template', 'style']
+                // order: ['script', 'template', 'style']
             }
         ]
     }
